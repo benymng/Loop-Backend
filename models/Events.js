c38210add9270c5ad4f34c5053e687992d88103b
benymng <benymng@gmail.com>
2022-07-09 13:41:20 -0600

    first commit

--- models/Events.js
+++ models/Events.js
@@ -1,5 +1,5 @@
 //this schema now includes image links for the blog
-
+const slugify = require("slugify");
 const mongoose = require("mongoose");
 
 // todo: find way to make event recurring, add tags (making array of interests?), number of attendees,
@@ -23,8 +23,8 @@ const Events = new mongoose.Schema({
     default: false,
   },
   image: {
-    data: Buffer,
-    contentType: String,
+    type: String,
+    required: true,
   },
   location: {
     type: String,
@@ -32,6 +32,18 @@ const Events = new mongoose.Schema({
   externalLinks: {
     type: String,
   },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+Events.pre("validate", function (next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+  next();
 });
 
 module.exports = mongoose.model("Events", Events);
