d4720277f25f449ddfb9b2eb69fbc97730f03c1d
benymng <benymng@gmail.com>
2022-07-15 20:34:22 -0600

    added localhost 3000 to access

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(cors());
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   next();
 });
 
